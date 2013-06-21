09135e1dbae69fcaf98aa2d6e5eda336214fb4ee
Hans Kuijpers <hans2103@gmail.com>
2013-06-21 10:17:16 +0200

    search tab and replace with 4 spaces

--- helper.php
+++ helper.php
@@ -2,11 +2,11 @@
 /**
  * Joomla! module Ecwid Product
  *
- * @author Hans2103 (info@hkweb.nl)
- * @package mod_ecwid_product
- * @copyright Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license GNU Public License
- * @link http://www.hkweb.nl
+ * @author     Hans2103 (info@hkweb.nl)
+ * @package    mod_ecwid_product
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU Public License
+ * @link       http://www.hkweb.nl
  */
 
 // No direct access
@@ -18,7 +18,7 @@ defined('_JEXEC') or die;
 abstract class modEcwidProductHelper
 {
 
-	private $data = null;
+    private $data = null;
 
     /*
      * Fetch the content from the bridge
@@ -29,19 +29,19 @@ abstract class modEcwidProductHelper
      */
     static public function build($params = null)
     {
-		$storeid	= $params->get('store_id');
-		$productid	= $params->get('product_id');
-		
-		// get Product data
-		$URLProduct	= 'http://app.ecwid.com/api/v1/' . $storeid . '/product?id=' . $productid;
-		$dataProduct = json_decode(file_get_contents($URLProduct),true);
-		
-		// get Profile data
-		$URLProfile = 'http://app.ecwid.com/api/v1/' . $storeid . '/profile';
-		$dataProfile = json_decode(mb_convert_encoding(file_get_contents($URLProfile),'HTML-ENTITIES','UTF-8'));
-		
-		// combine output
-		$data = (object) array_merge($dataProduct, (array) $dataProfile);
+        $storeid    = $params->get('store_id');
+        $productid    = $params->get('product_id');
+        
+        // get Product data
+        $URLProduct    = 'http://app.ecwid.com/api/v1/' . $storeid . '/product?id=' . $productid;
+        $dataProduct = json_decode(file_get_contents($URLProduct),true);
+        
+        // get Profile data
+        $URLProfile = 'http://app.ecwid.com/api/v1/' . $storeid . '/profile';
+        $dataProfile = json_decode(mb_convert_encoding(file_get_contents($URLProfile),'HTML-ENTITIES','UTF-8'));
+        
+        // combine output
+        $data = (object) array_merge($dataProduct, (array) $dataProfile);
 
         return $data;
         
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * @package		Joomla.Site
- * @subpackage	mod_ecwid_product
- * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package        Joomla.Site
+ * @subpackage     mod_ecwid_product
+ * @copyright      Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license        GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // no direct access
 defined('_JEXEC') or die;
 ?>
 <div class="ecwid-product<?php echo $moduleclass_sfx ?>">
-	<a title="<?php echo $data->name ?>" href="<?php echo $data->url ?>">
-		<img src="<?php echo $data->thumbnailUrl ?>" title="<?php echo $data->name ?>" alt="<?php echo $data->name ?>">
-	</a>
-	<div class="ecwid-product-name">
-		<a title="<?php echo $data->name ?>" href="<?php echo $data->url ?>"><?php echo $data->name ?></a>
-	</div>
-	<div class="ecwid-product-price"><?php echo $data->currencyPrefix ?><?php echo $data->price ?></div>
-	<div class="ecwid-product-add2cart">
-		<script type="text/javascript" src="http://app.ecwid.com/script.js?<?php echo $params->get('store_id') ?>" charset="utf-8"></script>
-		<script type="text/javascript">xAddToBag('productid=<?php echo $params->get('product_id') ?>');</script>
-	</div>
+    <a title="<?php echo $data->name ?>" href="<?php echo $data->url ?>">
+        <img src="<?php echo $data->thumbnailUrl ?>" title="<?php echo $data->name ?>" alt="<?php echo $data->name ?>">
+    </a>
+    <div class="ecwid-product-name">
+        <a title="<?php echo $data->name ?>" href="<?php echo $data->url ?>"><?php echo $data->name ?></a>
+    </div>
+    <div class="ecwid-product-price"><?php echo $data->currencyPrefix ?><?php echo $data->price ?></div>
+    <div class="ecwid-product-add2cart">
+        <script type="text/javascript" src="http://app.ecwid.com/script.js?<?php echo $params->get('store_id') ?>" charset="utf-8"></script>
+        <script type="text/javascript">xAddToBag('productid=<?php echo $params->get('product_id') ?>');</script>
+    </div>
 </div>
